5e3c26b5a279241a7a4cdd0f26337f95add72f51
schneeregenflocke <schneeregenflocke@gmail.com>
2021-01-06 09:30:43 +0100

    change the license from GPLv3 to GPLv2

--- src/graphics/shapes_base.h
+++ src/graphics/shapes_base.h
@@ -2,9 +2,9 @@
 Decade
 Copyright (c) 2019-2020 Marco Peyer
 
-This program is free software: you can redistribute it and/or modify
+This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
+the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -12,12 +12,12 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110 - 1301 USA.
 */
 
-#ifndef SHAPES_BASE_H
-#define SHAPES_BASE_H
+#pragma once
 
 #include "shaders.h"
 
@@ -283,6 +283,3 @@ vec4& Shape::GetColorRef(size_t index)
 		vertices[index + 2].normal = normal;
 	}
 }*/
-
-
-#endif /*SHAPES_BASE_H*/
