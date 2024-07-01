2313835d6bcf57b8b7e16c334af2d076f4725ebe
schneeregenflocke <schneeregenflocke@gmail.com>
2024-07-01 07:38:53 +0200

    #include <cstddef> // for offsetof

--- src/graphics/shaders.hpp
+++ src/graphics/shaders.hpp
@@ -38,6 +38,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <cstddef> // for offsetof
 
 #include "Resource.h"
 
